7afc9dae1c84d4567d96264b8094009560b1f7a3
Owaish Kalim <owaishkalim@gmail.com>
2021-07-19 17:23:01 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -2,30 +2,28 @@ import React, { useState, useMemo } from 'react'
 import Select from 'react-select'
 import countryList from 'react-select-country-list'
 import Pagination from './components/pagination';
+import Show from './components/Show';
 const axios = require('axios');
 
-const url = 'http://universities.hipolabs.com/search?country='
+const url = 'https://covid19-api.com/country?name='
 
 function App() {
   const [value, setValue] = useState('')
-  const [resp, setResp] = useState([]);
+  const [resp, setResp] = useState({});
   const options = useMemo(() => countryList().getData(), []) 
 
   const changeHandler = value => { 
     console.log(`Option selected:`, value.label) ;  
-    resp.splice(0, resp.length)    
-    var x = '' ;
+    setResp({}) ;
 
-    for (var i = 0; i < value.label.length; i++) 
-    if(value.label[i] === ' ') x += '+' ; 
-    else x += value.label[i] ;
-
-    const uri = url + x
+    const uri = url + value.label + '&format=json'
 
     axios.get(uri)
     .then(function (response) { 
-      var temp = resp.concat(response.data) 
+      console.log(response.data)
+      var temp = response.data 
       setResp(temp) ; 
+      console.log('Owaish', resp) 
     })
     .catch(function (error) {
       console.log(error);
@@ -33,16 +31,22 @@ function App() {
     setValue(value) 
   }
 
+  if(Object.keys(resp).length !== 0)
+  console.log('Kalim', resp[0].confirmed) 
+
   return (
     <div>  
       <div> 
-        <h2 style={{textAlign:'center'}}> Countrywise Universities list with its domains </h2>
+        <h1 style={{textAlign:'center'}}> Countrywise Covid19 Cases </h1>
       </div>
-     {value === '' ? (<h1 style={{textAlign:'center'}}> Select a Country </h1>) : ''}
+     {value === '' ? (<h2 style={{paddingLeft: '20px'}}> Select a Country </h2>) : ''}
+     <div style={{width:'40%', paddingLeft:'20px'}}> 
       <Select options={options} value={value} onChange={changeHandler} />
-      {value !== '' ? (<h1 style={{textAlign:'center'}}> Country : {value.label} </h1>) : ''} 
-      {value !== '' && resp.length === 0 && <h2 style={{textAlign:'center'}}> No Results Found </h2>}
-      {resp.length !== 0 &&  <Pagination data = {resp} /> }
+      </div>
+      {value !== '' ? (<h2 style={{paddingLeft: '20px'}}> Country : {value.label} </h2>) : ''} 
+      {Object.keys(resp).length !== 0 ? <Show data={resp[0]} /> : ''} 
+      {/* {value !== '' && resp.length === 0 && <h2 style={{textAlign:'center'}}> No Results Found </h2>} */}
+      {/* {resp.length !== 0 &&  <Pagination data = {resp} /> } */} 
     </div>
   )
   
